ee1ad6779eef23302a73205f9205456df3357daf
Sasha Turko <oleksandraturko9796@gmail.com>
2023-08-15 19:52:22 +0300

    add information

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict"
+
+let x = 5;
+let y = 3;
+
+let sum = x+y;
+console.log ("sum:", sum);
+
+let minus = x-y;
+console.log ("minus:", minus);
+
+let multiple = x*y;
+console.log ("multiple:", multiple);
+
+let division = x/y;
+console.log ("division:", division);
+
+let degree = x ** y;
+console.log ("degree:", degree);
